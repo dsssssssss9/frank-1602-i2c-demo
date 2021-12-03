ba57b8b4377d9d1dac49e0de23abcbcf3977e7cc
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-12-03 00:19:41 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,14 @@
 input.onButtonPressed(Button.A, function () {
     for (let index = 0; index < 16; index++) {
         I2C_LCD1602.shr()
+        basic.pause(100)
     }
     I2C_LCD1602.clear()
     I2C_LCD1602.ShowString("Regards", 16, 0)
     I2C_LCD1602.ShowString("Jason", 16, 1)
     for (let index = 0; index < 15; index++) {
         I2C_LCD1602.shl()
+        basic.pause(100)
     }
 })
 I2C_LCD1602.LcdInit(39)
